4daa74a12b1d1bf650359b994fe20f345f5e7e24
Mohammed Ali Agha <mtxshiftg@gmail.com>
2021-11-19 17:14:17 +0300

    initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { browser } from '$app/env';
+  import 'virtual:windi.css';
+  if (browser) import('virtual:windi-devtools');
+</script>
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Counter from '$components/Counter.svelte';
+</script>
+
+<template>
+  <main class="container py-4">
+    <h1 class="text-xl font-bold text-primary mb-1">SvelteKit App Template</h1>
+    <p class="mb-4">
+      An app template for building SvelteKit apps with WindiCSS and a reasonable
+      amount of boilerplate written for you.
+    </p>
+    <Counter />
+  </main>
+</template>
